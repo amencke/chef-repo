f03b74543d40e64c19e3b0dfd54ff4c968a41b2a
Arthur <arthur.mencke@gmail.com>
2016-04-22 13:15:06 +0100

    update vagrant configuration

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "amencke"
+client_key               "#{current_dir}/amencke.pem"
+validation_client_name   "devops-pro-validator"
+validation_key           "#{current_dir}/devops-pro-validator.pem"
+chef_server_url          "https://api.chef.io/organizations/devops-pro"
+cookbook_path            ["#{current_dir}/../cookbooks"]
